c9a4040fdc880dad3627692a9f286f6f4f196d48
Cecilio0 <drones9182@gmail.com>
2025-01-24 12:10:24 -0500

    Comments and cleanup

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 def extract_from_folder(folder: str):
     import os
 
+    # Extract data from csv files
     with open('output.txt', 'w', encoding='utf-8') as file:
         for file_name in os.listdir(folder):
             if file_name.endswith(".csv"):
                 file_path = f"{folder}/{file_name}"
                 extract_from_csv(file_path, file)
 
-    content: str = ""
+    # Clean output
     with open('output.txt', 'r', encoding='utf-8') as file:
         content = file.read()
 
@@ -15,21 +16,21 @@ def extract_from_folder(folder: str):
     with open('output.txt', 'w', encoding='utf-8') as file:
         file.write(content)
 
+    print('Listo el pollo')
+
 def extract_from_csv(file_path: str, file):
     import pandas as pd
     df = pd.read_csv(file_path)
-    print(df.keys()[0])
-    print(df.head())
     key = df.keys()[0]
+
+    # Extract data from csv
     df[key] = df[key].str.split('_').str[1]
+
+    # Write data to file
     for index, row in df.iterrows():
-        # print(f"{row['Collection: Etereo'][:5]} + ", end='')
         file.write(f"{row[key][:5]} + ")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     csv_dir = 'files'
     extract_from_folder(csv_dir)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
